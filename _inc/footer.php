fb1d00619e5eb4f9907c30b1e414e8fd3b931957
Leo Bauza <lb@hy.ly>
2013-01-30 22:59:29 -0500

    renaming files

--- _inc/footer.php
+++ _inc/footer.php
@@ -5,7 +5,7 @@
 	<!-- javascript -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script src="../assets/js/touchwipe.min.js"></script>
-	<script src="../assets/js/script.js"></script> 
+	<script src="../assets/js/malcolm.js"></script> 
 	<!-- end javascript -->
 </body>
 </html>
