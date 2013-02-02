1796b6892b75ec5b438a545a909d734a6ae9ad48
Leo <lbauza@saforian.com>
2013-02-02 04:25:42 -0500

    add modal to default

--- _inc/footer.php
+++ _inc/footer.php
@@ -7,16 +7,25 @@
 	<!-- javascript -->
 
 	<!-- why the fuck are these called modals? -->
-	<section data-modal="wiper" class="modal-main">
-		<header>
-			<h2>Wiper Modals</h2>
-		</header>
-		<article class="body">
-			<p>Write something that returns whether or not serenity, washer, and styles.css are cleared...</p>
-		</article>
-		<footer>
-			<p>modals can have footers</p>
-		</footer>
+	<?php
+	
+	//
+	
+	
+	?>
+
+	<section class="modal-cont">
+		<aside data-modal="wiper" class="modal-main ">
+			<header>
+				<h2>Wiper Modals</h2>
+			</header>
+			<article class="body">
+				<p>Write something that returns whether or not serenity, washer, and styles.css are cleared...</p>
+			</article>
+			<footer>
+				<p>modals can have footers</p>
+			</footer>
+		</aside>
 	</section>
 	<div class="overlay dark" data-modal='wiper'></div>
 
@@ -26,7 +35,11 @@
 
 
 	<script>
+		//hide all modals no matter their modal relation or if modal files are wri
+		$("[data-modal]").hide();
 		
+		
+		//show modal wipers on wiper clicks
 		$('[data-ajax="wiper"]').click(function(e){
 			
 			$("[data-modal='wiper']").show();
