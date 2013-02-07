cdd6ee077395221428ed55d0da5ef59b38027744
Leo <lbauza@saforian.com>
2013-02-07 10:34:39 -0500

    bcmodal plugin working branching to expand

--- assets/js/firefly.js
+++ assets/js/firefly.js
@@ -22,33 +22,7 @@
 
 (function( $ ) {
 	
-	var methods = {
-		init : function( options ) { 
-			// THIS 
-		},
-		show : function( ) {
-			// IS
-		},
-		hide : function( ) { 
-			// GOOD
-		},
-		update : function( content ) { 
-			// !!! 
-		}
-	};
-	
-	
 	$.fn.bcModals = function(options) {
-		
-		// Method calling logic (which I admit I dont understand AT ALL)
-		if ( methods[method] ) {
-			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		} else if ( typeof method === 'object' || ! method ) {
-			return methods.init.apply( this, arguments );
-		} else {
-			$.error( 'Method ' +	method + ' does not exist on jQuery.tooltip' );
-		}
-		
 		
 		
 		//default ID declared
