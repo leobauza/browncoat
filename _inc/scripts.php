1c1be6cd3675a07937db34273e8a035b64db66d6
Leo <lbauza@saforian.com>
2013-02-07 13:46:28 -0500

    moved bcModal to its own file

--- _inc/scripts.php
+++ _inc/scripts.php
@@ -1,4 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="../assets/js/touchwipe.min.js"></script>
 <script src="../assets/js/malcolm.js"></script> 
-<script src="../assets/js/firefly.js"></script> 
+<script src="../assets/js/bcModals.js"></script> 
