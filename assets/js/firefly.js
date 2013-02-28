60d7da7a22d4ac2a835a889c41462c0cc6137c7f
Leo <lbauza@saforian.com>
2013-02-28 14:52:50 -0500

    boilerplate for plugins and form plugin begins

--- assets/js/firefly.js
+++ assets/js/firefly.js
@@ -1,8 +1,7 @@
 ;(function ( $, window, document, undefined ) {
 
 	var 
-		pluginName = 'fireflyModal',
-		modalID = 0
+		pluginName = 'myplugin'
 	;
 
 	function Plugin( element, options ) {
@@ -10,71 +9,27 @@
 		this.element = element;
 		//used to set defaults and have the option of data from markup
 		var
-			$modal = $(this.element),
-			data = $modal.data(),
-			modalID = modalID + 1
+			$plugin = $(this.element),
+			data = $plugin.data()
 		;
 		//default settings :: data comes in plugin overlay event and id
 		this.options = $.extend( {
-			'modalOverlay' : (data.overlay) ? data.overlay : 'dark',
-			'modalEvent' : (data.event) ? data.event : 'click',
-			'modalID' : (data.id) ? data.id : modalID,
-			'modalHeight' : $modal.height(),
-			'modalParent' : $modal.closest('[data-role=parent]'),
+			'myoption' : (data.myoption) ? data.myoption : 'my default',
+			'myoption2' : (data.myoptions2) ? data.myoption2 : 'my default 2'
 		}, options) ;
 		this._name = pluginName;
 		this.init($modal, data);
 	}
+	
 	Plugin.prototype = {
-		init : function (modal, data) {
-			// this.element, this.option, modal is this.element object, data is the this.element.data()
-			var
-				//thePlugin = this,
-				$modal = $(this.element),
-				$parent = this.options.modalParent,
-				$trigger = this.options.modalParent.find('[data-role=trigger]'),
-				theEvent = this.options.modalEvent
-			;
-			//pass my init vars through to other events as need be
-			if(!theEvent || theEvent == "click") {
-				this.modalClick($modal, $parent, $trigger);
-			} else if(theEvent == "hover") {
-				this.modalHover($modal, $parent, $trigger);
-			} else {
-				this.modalLoad($modal, $parent);
-			}
+		init : function (stuff) {
+			//my init code
+			
+			//call my methods from init
+			this.myMethod(stuff);
 		}
-		, modalClick : function(modal, parent, trigger) {
-			var
-				that = this;
-			;
-			trigger.on("click",function(e){
-				$(this).closest('[data-role=parent]').find(modal).show().css('margin-top', -(that.options.height/2));
-				parent.after('<div id="'+ that.options.modalID +'" class="overlay ' + that.options.modalOverlay + '"></div>');
-				that.modalClose(modal, that.options.modalID);
-				e.preventDefault();
-			});
-		}
-		, modalHover : function(modal, parent, trigger){
-			trigger.hover(
-			function(){
-				$(this).closest('[data-role=parent]').find(modal).show();
-			},
-			function() {
-				$(this).closest('[data-role=parent]').find(modal).hide();
-			});
-		}
-		, modalLoad : function(modal, parent) {
-				modal.show().css('margin-top', -(this.options.modalHeight/2));
-				parent.after('<div id="'+ this.options.modalID +'" class="overlay ' + this.options.modalOverlay + '"></div>');
-				this.modalClose(modal, this.options.modalID);
-		}
-		, modalClose : function(modal, modalID) {
-			$("#" + modalID + ".overlay").on("click", function(e){
-				$(this).remove();
-				modal.hide();
-				e.preventDefault();
-			});
+		, myMethod : function(stuff) {
+			//my event
 		}
 	}
 
@@ -96,20 +51,12 @@
 ===========================================================
 	OPTIONS DEFAULTS:
 	
-	'modalOverlay' : (data.overlay) ? data.overlay : 'dark', //dark or light
-	'modalEvent' : (data.event) ? data.event : 'click', // click, hover, load
-	'modalID' : (data.id) ? data.id : modalID,
-	'modalHeight' : $modal.height(),
-	'modalParent' : $modal.closest('[data-role=parent]'),
+	'myoption' : (data.myoption) ? data.myoption : 'my default', 
+	'myoption2' : (data.myoption2) ? data.myoption2 : 'my default 2'
+	
  ===========================================================
 
  */
 
-//write data api
-
-$('[data-plugin=modal]').fireflyModal();
-
-
-
-
-
+//data api built into plugin
+$('[data-plugin=plugin]').myplugin();
