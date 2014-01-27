87a4e1d1067c3998b8289ecbf3680885d0b3f9b6
Leo <lbauza@saforian.com>
2014-01-27 10:03:28 -0500

    add new form styles

--- views/css/_forms.php
+++ views/css/_forms.php
@@ -1,7 +1,7 @@
 <section class="divider division">
 	<h3>Forms</h3>
 	<div class="callout--warning"><strong>browncoat</strong> is still in early development, forms are very unfinished.</div>
-	<form>
+	<!-- <form>
 		<legend>I am legend</legend>
 
 		<label>text</label>
@@ -41,5 +41,57 @@
 			<input type="radio" name="a" value="2" >
 			radio 2
 		</label>
-	</form>
+	</form> -->
+	
+
+	<?php
+	
+$form ='<form>
+	<legend>I am legend</legend>
+	<label>text</label>
+	<input type="text" placeholder="placeholder"></input>
+	<label>email</label>
+	<input type="email" placeholder="placeholder"></input>
+	<label>password</label>
+	<input type="password"></input>
+	<label>file</label>
+	<input type="file">
+	<label>textarea</label>
+	<textarea rows="3"></textarea>
+	<label>select</label>
+	<select>
+		<option>one</option>
+		<option>two</option>
+	</select>
+	<label>select multiple</label>
+	<select multiple>
+		<option>one</option>
+		<option>two</option>
+	</select>
+	<label>
+		<input type="checkbox" value="">
+		checkbox
+	</label>
+	<label>
+		<input type="radio" name="a" value="1" checked>
+		radio
+	</label>
+	<label>
+		<input type="radio" name="a" value="2" >
+		radio 2
+	</label>
+</form>';
+	
+	print $form;
+	
+	?>
+	<pre>
+		<?php
+		
+		print htmlentities($form);
+		
+		?>
+	</pre>
+	
+	
 </section>
