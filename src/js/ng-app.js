fbb8a0849cedbe3028ddf4f8480ef0d3148cf3a0
Leo Bauza <leob@njimedia.com>
2016-02-21 16:52:05 -0500

    task(angular) Add angular setup
    Add code samples folder. Add ng-templates folder. Add js angular bundle. Updated package.json to include angular and underscore as dependencies

---
+++ src/js/ng-app.js
@@ -0,0 +1,127 @@
+"use strict";
+
+var _ = require("underscore");
+
+// init app
+require("angular");
+require("angular-route");
+require("angular-sanitize");
+
+angular.module('app', ['ngRoute', 'ngSanitize']);
+var app = angular.module('app');
+
+app.service('Data', ['$q', '$http', function ($q, $http) {
+
+  this.get = function () {
+    var d = $q.defer();
+
+    // Get code
+    var code = $http.get("/code_samples/boilerplate.html", {
+      cache: true
+    });
+
+    var data = $http.get("/data/data.json", {
+      cache: true
+    });
+
+    $q.all([code, data]).then(function (results) {
+      var resolve = {
+        code: results[0].data,
+        data: results[1].data
+      };
+      d.resolve(resolve);
+    });
+
+    return d.promise;
+  };
+
+}]);
+
+app.controller('homeCtrl', ['$scope', '$timeout', 'data', function ($scope, $timeout, data) {
+
+  var rawCodeSamples = data.code.split("|||"),
+      codeSamples = {};
+
+  _.each(rawCodeSamples, function (v, k) {
+    var codeSample = v.split(":::");
+    codeSamples[codeSample[0].trim()] = codeSample[1].trim();
+  });
+
+  $scope.boilerplate = codeSamples.boilerplate;
+  $scope.other = codeSamples.heading;
+  $scope.data = data.data;
+
+  console.log(data.data);
+
+  $timeout(function () {
+    PR.prettyPrint();
+  }, 0);
+
+}]);
+
+app.config(["$routeProvider", "$locationProvider",
+function ($routeProvider, $locationProvider) {
+
+  $routeProvider
+    .when("/", {
+      templateUrl: "/ng-templates/home.html",
+      controller: "homeCtrl",
+      resolve: {
+        "data" : ["Data", function (Data) {
+          return Data.get().then(function (val) {
+            return val;
+          });
+        }]
+      },
+    });
+
+  $locationProvider.html5Mode(true).hashPrefix("!");
+
+}]);
+
+// filters
+// require('./filters');
+
+// directives
+// require('./directives');
+
+// services
+// require('./services');
+
+// controllers
+// require('./controllers');
+
+// routing
+// app.config(require('./config'));
+
+/**
+ * @ngdoc interface
+ * @name app.constant:BOOTSTRAP
+ * @description
+ * Bootstrapped data and methods ie a config constatnt.
+ */
+// app.constant('BOOTSTRAP', {
+//   page: bs.page,
+//   mainMenu: bs.menus.mainMenu,
+//   footerMenu: bs.menus.footerMenu,
+//   tpl: function (file) {
+//     return bs.tpls + "/assets/tpls/" + file + ".html";
+//   }
+// });
+
+// app.run(['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
+//
+//   $rootScope.$on('$locationChangeStart', function (e, next, prev) {
+//     // console.log(e);
+//     next = next.split("?")[0];
+//     prev = prev.split("?")[0];
+//     // Choose how to handle page changes here...
+//
+//     if (next !== prev) {
+//       $window.scrollTo(0,0);
+//     }
+//
+//   });
+//
+// }]);
+
